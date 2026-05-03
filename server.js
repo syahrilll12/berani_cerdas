403012a7491b275ea15e8f3f2b74c3ef9331227c
Syahkty <andisyahktu46@gmail.com>
2026-05-04 03:46:30 +0800

    memperbaiki kode

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const apiRoutes = require('./routes/api');
 
 const app = express();
+
+app.set('trust proxy', 1);
+
 const port = process.env.PORT || 3000;
 
 // Middleware global
